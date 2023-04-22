0e6b6a32235b52029fc6e6d1b2b2f2bcb8fce674
SeqSEE <SeqSEE@users.noreply.github.com>
2023-04-21 21:14:21 -0400

    fix eating

--- crafts.lua
+++ crafts.lua
@@ -1,17 +1,16 @@
-
 local S = mobs.intllib
 
 -- name tag
 minetest.register_craftitem("mobs:nametag", {
 	description = S("Name Tag"),
 	inventory_image = "mobs_nametag.png",
-	groups = {flammable = 2, nametag = 1}
+	groups = { flammable = 2, nametag = 1 }
 })
 
 if minetest.get_modpath("dye") and minetest.get_modpath("farming") then
 	minetest.register_craft({
 		output = "mobs:nametag",
-		recipe = {{"default:paper", "dye:black", "farming:string"}}
+		recipe = { { "default:paper", "dye:black", "farming:string" } }
 	})
 end
 
@@ -19,37 +18,37 @@ end
 minetest.register_craftitem("mobs:leather", {
 	description = S("Leather"),
 	inventory_image = "mobs_leather.png",
-	groups = {flammable = 2, leather = 1}
+	groups = { flammable = 2, leather = 1 }
 })
 
 -- raw meat
 minetest.register_craftitem("mobs:meat_raw", {
 	description = S("Raw Meat") .. '\n' ..
-    minetest.colorize('#DEB887', S('Hunger') .. ': 3'),
+		minetest.colorize('#DEB887', S('Hunger') .. ': 3'),
 	inventory_image = "mobs_meat_raw.png",
 	on_use = function(itemstack, user, pointed_thing)
 		local hunger_amount = minetest.get_item_group(itemstack:get_name(), "hunger_amount") or 0
-		if hunger_amount == 0 then 
+		if hunger_amount == 0 then
 			return itemstack
 		end
-		minetest.item_eat(hunger_amount)
+		return minetest.item_eat(hunger_amount)(itemstack, user, pointed_thing)
 	end,
-	groups = {food_meat_raw = 1, flammable = 2, hunger_amount = 3}
+	groups = { food_meat_raw = 1, flammable = 2, hunger_amount = 3 }
 })
 
 -- cooked meat
 minetest.register_craftitem("mobs:meat", {
 	description = S("Meat") .. '\n' ..
-    minetest.colorize('#DEB887', S('Hunger') .. ': 8'),
+		minetest.colorize('#DEB887', S('Hunger') .. ': 8'),
 	inventory_image = "mobs_meat.png",
 	on_use = function(itemstack, user, pointed_thing)
 		local hunger_amount = minetest.get_item_group(itemstack:get_name(), "hunger_amount") or 0
-		if hunger_amount == 0 then 
+		if hunger_amount == 0 then
 			return itemstack
 		end
-		minetest.item_eat(hunger_amount)
+		return minetest.item_eat(hunger_amount)(itemstack, user, pointed_thing)
 	end,
-	groups = {food_meat = 1, flammable = 2, hunger_amount = 8}
+	groups = { food_meat = 1, flammable = 2, hunger_amount = 8 }
 })
 
 minetest.register_craft({
@@ -63,17 +62,16 @@ minetest.register_craft({
 minetest.register_tool("mobs:lasso", {
 	description = S("Lasso (right-click animal to put in inventory)"),
 	inventory_image = "mobs_magic_lasso.png",
-	groups = {flammable = 2}
+	groups = { flammable = 2 }
 })
 
 if minetest.get_modpath("farming") then
-
 	minetest.register_craft({
 		output = "mobs:lasso",
 		recipe = {
-			{"farming:string", "", "farming:string"},
-			{"", "default:diamond", ""},
-			{"farming:string", "", "farming:string"}
+			{ "farming:string", "",                "farming:string" },
+			{ "",               "default:diamond", "" },
+			{ "farming:string", "",                "farming:string" }
 		}
 	})
 end
@@ -84,17 +82,16 @@ minetest.register_alias("mobs:magic_lasso", "mobs:lasso")
 minetest.register_tool("mobs:net", {
 	description = S("Net (right-click animal to put in inventory)"),
 	inventory_image = "mobs_net.png",
-	groups = {flammable = 2}
+	groups = { flammable = 2 }
 })
 
 if minetest.get_modpath("farming") then
-
 	minetest.register_craft({
 		output = "mobs:net",
 		recipe = {
-			{"group:stick", "", "group:stick"},
-			{"group:stick", "", "group:stick"},
-			{"farming:string", "group:stick", "farming:string"}
+			{ "group:stick",    "",            "group:stick" },
+			{ "group:stick",    "",            "group:stick" },
+			{ "farming:string", "group:stick", "farming:string" }
 		}
 	})
 end
@@ -103,14 +100,14 @@ end
 minetest.register_tool("mobs:shears", {
 	description = S("Steel Shears (right-click to shear)"),
 	inventory_image = "mobs_shears.png",
-	groups = {flammable = 2}
+	groups = { flammable = 2 }
 })
 
 minetest.register_craft({
 	output = "mobs:shears",
 	recipe = {
-		{"", "default:steel_ingot", ""},
-		{"", "group:stick", "default:steel_ingot"}
+		{ "", "default:steel_ingot", "" },
+		{ "", "group:stick",         "default:steel_ingot" }
 	}
 })
 
@@ -118,15 +115,15 @@ minetest.register_craft({
 minetest.register_craftitem("mobs:protector", {
 	description = S("Mob Protection Rune"),
 	inventory_image = "mobs_protector.png",
-	groups = {flammable = 2}
+	groups = { flammable = 2 }
 })
 
 minetest.register_craft({
 	output = "mobs:protector",
 	recipe = {
-		{"default:stone", "default:stone", "default:stone"},
-		{"default:stone", "default:goldblock", "default:stone"},
-		{"default:stone", "default:stone", "default:stone"}
+		{ "default:stone", "default:stone",     "default:stone" },
+		{ "default:stone", "default:goldblock", "default:stone" },
+		{ "default:stone", "default:stone",     "default:stone" }
 	}
 })
 
@@ -134,15 +131,15 @@ minetest.register_craft({
 minetest.register_craftitem("mobs:protector2", {
 	description = S("Mob Protection Rune (Level 2)"),
 	inventory_image = "mobs_protector2.png",
-	groups = {flammable = 2}
+	groups = { flammable = 2 }
 })
 
 minetest.register_craft({
 	output = "mobs:protector2",
 	recipe = {
-		{"mobs:protector", "default:mese_crystal", "mobs:protector"},
-		{"default:mese_crystal", "default:diamondblock", "default:mese_crystal"},
-		{"mobs:protector", "default:mese_crystal", "mobs:protector"}
+		{ "mobs:protector",       "default:mese_crystal", "mobs:protector" },
+		{ "default:mese_crystal", "default:diamondblock", "default:mese_crystal" },
+		{ "mobs:protector",       "default:mese_crystal", "mobs:protector" }
 	}
 })
 
@@ -150,15 +147,15 @@ minetest.register_craft({
 minetest.register_craftitem("mobs:saddle", {
 	description = S("Saddle"),
 	inventory_image = "mobs_saddle.png",
-	groups = {flammable = 2, saddle = 1}
+	groups = { flammable = 2, saddle = 1 }
 })
 
 minetest.register_craft({
 	output = "mobs:saddle",
 	recipe = {
-		{"mobs:leather", "mobs:leather", "mobs:leather"},
-		{"mobs:leather", "default:steel_ingot", "mobs:leather"},
-		{"mobs:leather", "default:steel_ingot", "mobs:leather"}
+		{ "mobs:leather", "mobs:leather",        "mobs:leather" },
+		{ "mobs:leather", "default:steel_ingot", "mobs:leather" },
+		{ "mobs:leather", "default:steel_ingot", "mobs:leather" }
 	}
 })
 
@@ -167,51 +164,50 @@ minetest.register_craft({
 local mod_def = minetest.get_modpath("default")
 
 if mod_def and default.register_fence then
-
--- mob fence (looks like normal fence but collision is 2 high)
-default.register_fence("mobs:fence_wood", {
-	description = S("Mob Fence"),
-	texture = "default_wood.png",
-	material = "default:fence_wood",
-	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
-	sounds = mod_def and default.node_sound_wood_defaults(),
-	collision_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.5, 0.5, 1.9, 0.5},
+	-- mob fence (looks like normal fence but collision is 2 high)
+	default.register_fence("mobs:fence_wood", {
+		description = S("Mob Fence"),
+		texture = "default_wood.png",
+		material = "default:fence_wood",
+		groups = { choppy = 2, oddly_breakable_by_hand = 2, flammable = 2 },
+		sounds = mod_def and default.node_sound_wood_defaults(),
+		collision_box = {
+			type = "fixed",
+			fixed = {
+				{ -0.5, -0.5, -0.5, 0.5, 1.9, 0.5 },
+			}
 		}
-	}
-})
+	})
 end
 
 -- mob fence top (has enlarged collisionbox to stop mobs getting over)
 minetest.register_node("mobs:fence_top", {
 	description = S("Mob Fence Top"),
 	drawtype = "nodebox",
-	tiles = {"default_wood.png"},
+	tiles = { "default_wood.png" },
 	paramtype = "light",
 	is_ground_content = false,
-	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	groups = { choppy = 2, oddly_breakable_by_hand = 2, flammable = 2 },
 	sounds = mod_def and default.node_sound_wood_defaults(),
 	node_box = {
 		type = "fixed",
-		fixed = {-0.2, -0.5, -0.2, 0.2, 0, 0.2}
+		fixed = { -0.2, -0.5, -0.2, 0.2, 0, 0.2 }
 	},
 	collision_box = {
 		type = "fixed",
-		fixed = {-0.4, -1.5, -0.4, 0.4, 0, 0.4}
+		fixed = { -0.4, -1.5, -0.4, 0.4, 0, 0.4 }
 	},
 	selection_box = {
 		type = "fixed",
-		fixed = {-0.4, -1.5, -0.4, 0.4, 0, 0.4}
+		fixed = { -0.4, -1.5, -0.4, 0.4, 0, 0.4 }
 	}
 })
 
 minetest.register_craft({
 	output = "mobs:fence_top 12",
 	recipe = {
-		{"group:wood", "group:wood", "group:wood"},
-		{"", "default:fence_wood", ""}
+		{ "group:wood", "group:wood",         "group:wood" },
+		{ "",           "default:fence_wood", "" }
 	}
 })
 
@@ -270,10 +266,8 @@ minetest.register_tool(":mobs:mob_reset_stick", {
 	description = S("Mob Reset Stick"),
 	inventory_image = "default_stick.png^[colorize:#ff000050",
 	stack_max = 1,
-	groups = {not_in_creative_inventory = 1},
-
+	groups = { not_in_creative_inventory = 1 },
 	on_use = function(itemstack, user, pointed_thing)
-
 		if pointed_thing.type ~= "object" then
 			return
 		end
@@ -285,12 +279,10 @@ minetest.register_tool(":mobs:mob_reset_stick", {
 
 		-- spawn same mob with saved stats, with random texture
 		if obj and not sneak then
-
 			local self = obj:get_luaentity()
 			local obj2 = minetest.add_entity(obj:get_pos(), self.name)
 
 			if obj2 then
-
 				local ent2 = obj2:get_luaentity()
 
 				ent2.protected = self.protected
@@ -302,10 +294,10 @@ minetest.register_tool(":mobs:mob_reset_stick", {
 				ent2.order = self.order
 
 				if self.child then
-					obj2:set_velocity({x = 0, y = self.jump_height, z = 0})
+					obj2:set_velocity({ x = 0, y = self.jump_height, z = 0 })
 				end
 
-				obj2:set_properties({nametag = self.nametag})
+				obj2:set_properties({ nametag = self.nametag })
 
 				obj:remove()
 			end
@@ -313,7 +305,6 @@ minetest.register_tool(":mobs:mob_reset_stick", {
 
 		-- display form to enter texture name ending in .png
 		if obj and sneak then
-
 			tex_obj = obj
 
 			-- get base texture
@@ -326,24 +317,22 @@ minetest.register_tool(":mobs:mob_reset_stick", {
 			local name = user:get_player_name()
 
 			minetest.show_formspec(name, "mobs_texture", "size[8,4]"
-			.. "field[0.5,1;7.5,0;name;"
-			.. minetest.formspec_escape(S("Enter texture:")) .. ";" .. bt .. "]"
-			.. "button_exit[2.5,3.5;3,1;mob_texture_change;"
-			.. minetest.formspec_escape(S("Change")) .. "]")
+				.. "field[0.5,1;7.5,0;name;"
+				.. minetest.formspec_escape(S("Enter texture:")) .. ";" .. bt .. "]"
+				.. "button_exit[2.5,3.5;3,1;mob_texture_change;"
+				.. minetest.formspec_escape(S("Change")) .. "]")
 		end
 	end
 })
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
-
 	-- right-clicked with nametag and name entered?
 	if formname == "mobs_texture"
-	and fields.name
-	and fields.name ~= "" then
-
+		and fields.name
+		and fields.name ~= "" then
 		-- does mob still exist?
 		if not tex_obj
-		or not tex_obj:get_luaentity() then
+			or not tex_obj:get_luaentity() then
 			return
 		end
 
@@ -362,9 +351,9 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 		-- update texture
 		local self = tex_obj:get_luaentity()
 
-		self.base_texture = {fields.name}
+		self.base_texture = { fields.name }
 
-		tex_obj:set_properties({textures = {fields.name}})
+		tex_obj:set_properties({ textures = { fields.name } })
 
 		-- reset external variable
 		tex_obj = nil
@@ -375,54 +364,54 @@ end)
 -- Meat Block
 minetest.register_node("mobs:meatblock", {
 	description = S("Meat Block") .. '\n' ..
-    minetest.colorize('#DEB887', S('Hunger') .. ': 20'),
-	tiles = {"mobs_meat_top.png", "mobs_meat_bottom.png", "mobs_meat_side.png"},
+		minetest.colorize('#DEB887', S('Hunger') .. ': 20'),
+	tiles = { "mobs_meat_top.png", "mobs_meat_bottom.png", "mobs_meat_side.png" },
 	paramtype2 = "facedir",
-	groups = {choppy = 1, oddly_breakable_by_hand = 1, flammable = 2, hunger_amount = 20},
+	groups = { choppy = 1, oddly_breakable_by_hand = 1, flammable = 2, hunger_amount = 20 },
 	sounds = mod_def and default.node_sound_leaves_defaults(),
 	on_place = minetest.rotate_node,
 	on_use = function(itemstack, user, pointed_thing)
 		local hunger_amount = minetest.get_item_group(itemstack:get_name(), "hunger_amount") or 0
-		if hunger_amount == 0 then 
+		if hunger_amount == 0 then
 			return itemstack
 		end
-		minetest.item_eat(hunger_amount)
+		return minetest.item_eat(hunger_amount)(itemstack, user, pointed_thing)
 	end
 })
 
 minetest.register_craft({
 	output = "mobs:meatblock",
 	recipe = {
-		{"group:food_meat", "group:food_meat", "group:food_meat"},
-		{"group:food_meat", "group:food_meat", "group:food_meat"},
-		{"group:food_meat", "group:food_meat", "group:food_meat"}
+		{ "group:food_meat", "group:food_meat", "group:food_meat" },
+		{ "group:food_meat", "group:food_meat", "group:food_meat" },
+		{ "group:food_meat", "group:food_meat", "group:food_meat" }
 	}
 })
 
 -- Meat Block (raw)
 minetest.register_node("mobs:meatblock_raw", {
 	description = S("Raw Meat Block") .. '\n' ..
-    minetest.colorize('#DEB887', S('Hunger') .. ': 20'),
-	tiles = {"mobs_meat_raw_top.png", "mobs_meat_raw_bottom.png", "mobs_meat_raw_side.png"},
+		minetest.colorize('#DEB887', S('Hunger') .. ': 20'),
+	tiles = { "mobs_meat_raw_top.png", "mobs_meat_raw_bottom.png", "mobs_meat_raw_side.png" },
 	paramtype2 = "facedir",
-	groups = {choppy = 1, oddly_breakable_by_hand = 1, flammable = 2, hunger_amount = 20},
+	groups = { choppy = 1, oddly_breakable_by_hand = 1, flammable = 2, hunger_amount = 20 },
 	sounds = mod_def and default.node_sound_leaves_defaults(),
 	on_place = minetest.rotate_node,
 	on_use = function(itemstack, user, pointed_thing)
 		local hunger_amount = minetest.get_item_group(itemstack:get_name(), "hunger_amount") or 0
-		if hunger_amount == 0 then 
+		if hunger_amount == 0 then
 			return itemstack
 		end
-		minetest.item_eat(hunger_amount)
+		return minetest.item_eat(hunger_amount)(itemstack, user, pointed_thing)
 	end
 })
 
 minetest.register_craft({
 	output = "mobs:meatblock_raw",
 	recipe = {
-		{"group:food_meat_raw", "group:food_meat_raw", "group:food_meat_raw"},
-		{"group:food_meat_raw", "group:food_meat_raw", "group:food_meat_raw"},
-		{"group:food_meat_raw", "group:food_meat_raw", "group:food_meat_raw"}
+		{ "group:food_meat_raw", "group:food_meat_raw", "group:food_meat_raw" },
+		{ "group:food_meat_raw", "group:food_meat_raw", "group:food_meat_raw" },
+		{ "group:food_meat_raw", "group:food_meat_raw", "group:food_meat_raw" }
 	}
 })
 
